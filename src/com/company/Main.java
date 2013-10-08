491811e18a9a90aed429525d3182e9a5d22bc772
haxxed <aliashaxxed@gmail.com>
2013-10-08 18:19:59 -0500

    nananan

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,20 +1,24 @@
 package com.company;
+
 import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-       askName();
-       List nameList = new ArrayList();                    // create arrayList
+        askName();
+        List nameList = new ArrayList();                    // create arrayList
     }
-    public static void askName(){                   // asks for name
+
+    public static void askName() {                   // asks for name
         System.out.println("Please input a name to add to the list: ");
         Scanner scan = new Scanner(System.in);
         String tempName = scan.nextLine();
-        System.out.println("You added the name, "+tempName);
+        System.out.println("You added the name, " + tempName);
         addName(tempName);              // adds name to array
-        System.out.println("The current list of names is: "+nameList);  // prints array
+        System.out.println("The current list of names is: " + nameList);  // prints array
     }
-    public static void addName(){
 
+    public static void addName() {
+        // this is a test of the emergency broadcasting system
     }
 
 }
