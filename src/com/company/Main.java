abe1fa32a5ef3e5fb67a1952f5e51761aff3818d
haxxed <aliashaxxed@gmail.com>
2013-10-09 14:45:39 -0500

    adding ability to write to file - not going well.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,19 +1,20 @@
 package com.company;
 
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileWriter;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static ArrayList nameList = new ArrayList();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         askName();
     }
 
-    public static void askName() {                   // asks for name
+    public static void askName() throws IOException {                   // asks for name
         System.out.println("Please input a name to add to the list: ");
         Scanner scan = new Scanner(System.in);
         String tempName = scan.nextLine();
@@ -23,20 +24,20 @@ public class Main {
         askName();
     }
 
-    public static void addName(String tN) {
+    public static void addName(String tN) throws IOException {
         nameList.add(tN);
-        fileWrite(nameList);
+        fileWrite();
     }
 
-    public static void fileWrite() {
-        File file = new File("/%appdata%/JavaProg/TestFile/nameList.txt");
+    public static void fileWrite() throws IOException {
+        File file = new File("/%appdata%/JavaFiles/Taylor.txt");
         if (!file.exists()) {
+            file.mkdirs();
             file.createNewFile();
         }
-        FileWriter fw = new FileWriter(file.getAbsoluteFile());
-        BufferedWriter bw = new BufferedWriter(fw);
-        bw.write(nameList);
-        bw.close();
+        FileOutputStream fileOut = new FileOutputStream(file);
+        ObjectOutputStream objOut = new ObjectOutputStream(fileOut);
+        objOut.writeObject(nameList);
+        fileOut.close();
     }
-}
 }
